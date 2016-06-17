f36c0656ce5328700066ed5f1cc165b75b331711
Kenta Murata <mrkn@mrkn.jp>
2016-06-17 09:48:23 +0900

    :hatching_chick: bundle gem iruby-rails

---
+++ lib/iruby/rails.rb
@@ -0,0 +1,7 @@
+require "iruby/rails/version"
+
+module Iruby
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/iruby/rails/version.rb
@@ -0,0 +1,5 @@
+module Iruby
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
